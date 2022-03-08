4409d6677a4c2ff358324ee1bbdf15509f34542c
anathan <anathan.gv@gmail.com>
2022-03-07 23:10:10 -0300

    feat: completed application

--- sagaapp/src/App.js
+++ sagaapp/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
+import {getUsersFetch} from './actions'
 
 function App() {
+
+  const dispatch = useDispatch();
+  const users = useSelector(state => state.myFirstReducer.users)
+  
   return (
     <div className="App">
-     <h1>teste</h1>
+      <button onClick={() => dispatch(getUsersFetch())}>Get Users</button>
+      <div>Users: {users.map(user => (<div>{user.name}</div>))}</div>
     </div>
   );
 }
--- sagaapp/src/index.js
+++ sagaapp/src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+//redux
+import {Provider} from 'react-redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import {myFirstReducer} from './reducer';
+import createSagaMiddleware from 'redux-saga'
+import {mySaga} from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const rootReducer = combineReducers({ myFirstReducer });
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(mySaga);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
